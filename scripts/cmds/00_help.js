1f3d8a9a75c1379437cd84646b8ecd4ff6ffffd5
LordKing2 <nonk43798@gmail.com>
2024-09-02 19:21:27 +0200

    Update 00_help.js

--- scripts/cmds/00_help.js
+++ scripts/cmds/00_help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🇵🇭|𝗬𝗔𝗭𝗞𝗬 𝗔𝗜";
+const doNotDelete = "🇵🇭|༒☞🕊𝐿𝑜𝑟𝑑🕊|𖣘|🕊𝑆𝑖𝑙𝑒𝑛𝑡🕊𝑠𝑡𝑢𝑑𝑒𝑛𝑡🕊☜༒ 𝗔𝗜";
 /** 
 * @author NTKhang
 * @author: do not delete it
